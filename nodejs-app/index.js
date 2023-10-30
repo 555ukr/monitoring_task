f5a9ef2be0b7f14102084a7ece896c6967b03a55
Dmytro.Kalashnyk <dmytro.kalashnyk@sinch.com>
2023-10-30 11:44:47 +0100

    add metrics to telegraf agent

--- nodejs-app/index.js
+++ nodejs-app/index.js
@@ -4,10 +4,7 @@ const mongoose = require('mongoose');
 const { Client } = require('@elastic/elasticsearch');
 const port = 4000
 
-const esClient = new Client({ node: 'http://elasticsearch:9200', auth: {
-    username: 'admin',
-    password: 'password',
-  },}, );
+const esClient = new Client({ node: 'http://elasticsearch:9200'}, );
 
 mongoose.connect('mongodb://root:example@mongo:27017/', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', (error) => {
@@ -15,21 +12,21 @@ mongoose.connection.on('error', (error) => {
   process.exit(1);
 });
 
-async function createIndex() {
-    const indexName = 'your_index_name';
-  
-    try {
-      const response = await client.indices.create({
-        index: indexName,
-      });
-  
-      console.log(`Index created: ${response.body.index}`);
-    } catch (error) {
-      console.error(`Error creating index: ${error}`);
-    }
-  }
-  
-  createIndex();
+// async function createIndex() {
+//     const indexName = 'your_index_name';
+//
+//     try {
+//       const response = await esClient.indices.create({
+//         index: indexName,
+//       });
+//
+//       console.log(`Index created: ${response.body.index}`);
+//     } catch (error) {
+//       console.error(`Error creating index: ${error}`);
+//     }
+//   }
+//
+//   createIndex();
 
 const Document = mongoose.model('Document', {
     content: String,
@@ -46,16 +43,18 @@ const Document = mongoose.model('Document', {
       console.log(`Saved document with ID ${savedDocument._id}`);
 
       const dataToPush = {
-        index: 'your_index_name', // Replace with your Elasticsearch index
-        id: savedDocument._id, // Use the MongoDB document's _id as the Elasticsearch document ID
-        body: savedDocument,
+        index: 'some-data', // Replace with your Elasticsearch index
+        // id: savedDocument._id, // Use the MongoDB document's _id as the Elasticsearch document ID
+        body: {
+          content,
+        },
       };
 
       const response = await esClient.index(dataToPush);
-      console.log(`Document indexed successfully in Elasticsearch. Document ID: ${response.body._id}`);
+      console.log(`Document indexed successfully in Elasticsearch. Document ID: ${response?._id}`);
 
 
-      return {mongo: savedDocument._id, elastic: response.body._id}
+      return {mongo: savedDocument._id, elastic: response?._id}
     } catch (err) {
       console.error('Error saving document:', err);
       return null
